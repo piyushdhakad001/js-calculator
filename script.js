95fe3e28dcfcfd45cbebcae312af96dc5e52ef2a
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-06-29 12:00:25 +0530

    Add live calculation preview

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const calculationBox = document.querySelector('.calculation-box');
+const liveAnswerBox = document.querySelector('.answer-box');
 const isEqualButton = document.querySelector(".is-equal-button");
 
  let calculation = '';
-
+const operators = ['+', '÷', '×', '−'];
 
 const button = document.querySelectorAll(".button");
 
 button.forEach(button => {
-
   button.addEventListener('click', () => {
     addClickedButton(button);
   });
 })
+
 
 function toMathExpression(str){
  return str
@@ -23,6 +24,28 @@ function toMathExpression(str){
 function addClickedButton(button){
  calculation += button.textContent;
 calculationBox.textContent = calculation;
+
+// LONG CODE
+//  let hasOperator = false;
+// for(let i = 0; i < operators.length; i++) {
+//   if(calculation.includes(operators[i])) {
+//     hasOperator = true;
+//     break;
+//   }
+// }
+
+// SHORTHAND
+const hasOperator = operators.some(op =>
+calculation.includes(op)
+ );
+
+  if (hasOperator) {
+    try {
+      liveAnswerBox.textContent = eval(toMathExpression(calculation)); // live result
+    } catch {
+      liveAnswerBox.textContent = '...'; // incomplete expression like "5+"
+    }
+  }
 }
 
 isEqualButton.addEventListener('click', () => {
